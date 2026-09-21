1f08be2517da7aadf5520a1b5f595851da4c0523
Tanmay Pansare <tanmaypansare708@gmail.com>
2026-09-21 07:48:40 +0530

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int vote = 0;
+        int candidate = 0;
+        for(int num : nums){
+            if(vote == 0){
+                candidate = num;
+            }
+            if(candidate == num){
+                vote++;
+            }else vote--;
+        }
+        return candidate;
+    }
+}
